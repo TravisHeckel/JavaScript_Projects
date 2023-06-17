6258b6a6f1af1c65df41f5c519b85884e0edafec
TravisHeckel <Traheck@gmail.com>
2023-06-17 10:49:19 -0600

    included the two main folders

---
+++ Basic_JavaScript_Projects/Project1_expressions_alert/JS/main.js
@@ -0,0 +1,35 @@
+// this prints out any text listed in it.
+document.write("Hello World!");
+
+//This is a variable assignment that shown in a popup window.
+var warning = "Crap I forgot";
+
+window.alert(warning);
+
+
+//This is an example of a string AND typing out the variable on the page.
+var A =" This is a string!";
+
+document.write(A);
+
+//This reflects using the \ space to add a unique character as a piece of text.
+var B = " This isn\'t quite what I was expecting.";
+
+document.write (B);
+
+//This shows how a string can be put together using plus signs. It will add it right on next to the last text in the lineup.
+var C =" But I do enjoy what I do"
++ " sometimes its hard to remember that this isn\'t for fun,"
++ " but is actual work I must do to take care of my family.";
+document.write (C);
+
+// this shows how we only need to use declaration of var once and we can assign multiple variables. 
+document.write ("I almost forgot but my favorite pet is a: ");
+
+var D = "cats", E = "dogs", F="birds", G="rats"
+
+document.write(F);
+
+//This shows a statement that has an expression in it.
+
+document.write(3+3);
