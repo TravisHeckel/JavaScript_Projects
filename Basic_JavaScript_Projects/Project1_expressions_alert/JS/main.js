a1240ed7b079660b4321f4b8d0882c83fffd2876
TravisHeckel <Traheck@gmail.com>
2023-06-17 11:31:05 -0600

    updated the first assignment
    commented out un-necessary info and marked  the specific example required.

--- Basic_JavaScript_Projects/Project1_expressions_alert/JS/main.js
+++ Basic_JavaScript_Projects/Project1_expressions_alert/JS/main.js
@@ -1,35 +1,50 @@
 // this prints out any text listed in it.
-document.write("Hello World!");
+//document.write("Hello World!");
 
 //This is a variable assignment that shown in a popup window.
-var warning = "Crap I forgot";
+//var warning = "Crap I forgot";
 
-window.alert(warning);
+//window.alert(warning);
 
 
 //This is an example of a string AND typing out the variable on the page.
-var A =" This is a string!";
+//var A =" This is a string!";
 
-document.write(A);
+//document.write(A);
 
 //This reflects using the \ space to add a unique character as a piece of text.
-var B = " This isn\'t quite what I was expecting.";
+//var B = " This isn\'t quite what I was expecting.";
 
-document.write (B);
+//document.write (B);
 
 //This shows how a string can be put together using plus signs. It will add it right on next to the last text in the lineup.
-var C =" But I do enjoy what I do"
-+ " sometimes its hard to remember that this isn\'t for fun,"
-+ " but is actual work I must do to take care of my family.";
-document.write (C);
+//var C =" But I do enjoy what I do"
+//+ " sometimes its hard to remember that this isn\'t for fun,"
+//+ " but is actual work I must do to take care of my family.";
+//document.write (C);
 
 // this shows how we only need to use declaration of var once and we can assign multiple variables. 
-document.write ("I almost forgot but my favorite pet is a: ");
+//document.write ("I almost forgot but my favorite pet is a: ");
 
-var D = "cats", E = "dogs", F="birds", G="rats"
+//var D = "cats", E = "dogs", F="birds", G="rats"
 
-document.write(F);
+//document.write(F);
 
 //This shows a statement that has an expression in it.
 
-document.write(3+3);
+//document.write(3+3);
+
+
+//-----------------------first practice assignment---------------------
+
+//set each variable with a string.
+var Sent1 = ("This is the beginning of the string");
+var Sent2 = (" and this is the end of the string");
+ 
+
+//Window alert with a reminder
+window.alert("Your doing great!");
+
+//print a concatenate statement with variable Sent1 and Sent2
+document.write (Sent1 + Sent2);
+
